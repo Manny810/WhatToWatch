21f8576cd4f429a6178fd95aab5359558d81484e
Manny810 <mannyfavela2000@gmail.com>
2020-07-13 15:50:30 -0500

    changing to an empty project

--- app/src/androidTest/java/com/example/whattowatch/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/whattowatch/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.instagramclone;
+package com.example.whattowatch;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.instagramclone", appContext.getPackageName());
+        assertEquals("com.example.whattowatch", appContext.getPackageName());
     }
 }
