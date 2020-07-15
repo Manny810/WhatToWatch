d9fa6290694f434a3f4fba706325b2e744b87fb7
Manny810 <mannyfavela2000@gmail.com>
2020-07-15 12:55:51 -0500

    implemented camera activity

--- app/src/main/java/com/example/whattowatch/fragments/AccountFragment.java
+++ app/src/main/java/com/example/whattowatch/fragments/AccountFragment.java
@@ -1,11 +1,18 @@
 package com.example.whattowatch.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +20,17 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.whattowatch.R;
 import com.example.whattowatch.models.User;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +73,7 @@ public class AccountFragment extends Fragment {
             Log.d(TAG, "Profile photo not provided");
         }
 
-
     }
+
+
 }
