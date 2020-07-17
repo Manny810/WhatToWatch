be99ed1f60007ec10463f850c13c07187e87ba0e
Manny810 <mannyfavela2000@gmail.com>
2020-07-17 13:37:34 -0500

    creating the view and visual components of newMovieListActivity

--- app/src/main/java/com/example/whattowatch/NewMovieListActivity.java
+++ app/src/main/java/com/example/whattowatch/NewMovieListActivity.java
@@ -1,8 +1,10 @@
 package com.example.whattowatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.whattowatch.models.Movie;
 
@@ -11,8 +13,15 @@ import java.util.List;
 
 public class NewMovieListActivity extends AppCompatActivity {
 
+    /**
+     * NewMovieListActivity is the activity responsible for creating a new movie list
+     * The app goes to this activity when the user is trying to create a new movie list
+     */
     public static final String TAG = "NewMovieListActivity";
     List<Movie> movies;
+
+    Button btnFindMovie;
+    RecyclerView rvMovieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,8 @@ public class NewMovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_movie_list);
 
         movies = new ArrayList<Movie>();
+        btnFindMovie = findViewById(R.id.btnFindMovie);
+        rvMovieList = findViewById(R.id.rvMovieList);
 
     }
 }
