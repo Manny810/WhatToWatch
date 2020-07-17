a21b3de3629f0a94c2d60099f3eb49e43c55637f
Manny810 <mannyfavela2000@gmail.com>
2020-07-17 13:45:18 -0500

    creating findMovieActivity to find movies to add to movielist

---
+++ app/src/main/java/com/example/whattowatch/FindMovieActivity.java
@@ -0,0 +1,14 @@
+package com.example.whattowatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_movie);
+    }
+}
