dd4f96c63755f6361f30cb802f10fa577e0ce637
Manny810 <mannyfavela2000@gmail.com>
2020-07-20 16:57:00 -0500

    set up recycler view in movieListFragment with new adapter, and created function queryLists which pulls all movieLists in the database

--- app/src/main/java/com/example/whattowatch/fragments/MovieListFragment.java
+++ app/src/main/java/com/example/whattowatch/fragments/MovieListFragment.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +18,26 @@ import android.widget.Button;
 import com.example.whattowatch.CameraActivity;
 import com.example.whattowatch.NewMovieListActivity;
 import com.example.whattowatch.R;
+import com.example.whattowatch.adapters.MovieListAdapter;
+import com.example.whattowatch.adapters.NewMovieListAdapter;
+import com.example.whattowatch.models.MovieList;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MovieListFragment extends Fragment {
+    public static final String TAG = "MovieListFragment";
 
     RecyclerView rvMovieLists;
     Button btnNewList;
+
+    MovieListAdapter movieAdapter;
+    List<MovieList> movieLists;
 
     public MovieListFragment() {
         // Required empty public constructor
@@ -43,6 +58,7 @@ public class MovieListFragment extends Fragment {
         ParseUser user = ParseUser.getCurrentUser();
 
         btnNewList = view.findViewById(R.id.btnNewList);
+        rvMovieLists = view.findViewById(R.id.rvMovieLists);
 
         btnNewList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +68,37 @@ public class MovieListFragment extends Fragment {
             }
         });
 
+        movieLists = new ArrayList<>();
+
+        // create adapter for RecyclerView
+        movieAdapter = new MovieListAdapter(getContext(), movieLists);
+
+        // set the adapter as the recycler view adapter
+        rvMovieLists.setAdapter(movieAdapter);
+
+        // Set a layout Manager on the recycler view
+        rvMovieLists.setLayoutManager(new LinearLayoutManager(getContext()));
+
+    }
+
+    protected void queryLists() {
+        // Specify which class to query
+        ParseQuery<MovieList> query = ParseQuery.getQuery(MovieList.class);
+        query.include(MovieList.KEY_USER);
+        query.addDescendingOrder(MovieList.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<MovieList>() {
+            @Override
+            public void done(List<MovieList> movieLists, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (MovieList movieList: movieLists){
+                    Log.i(TAG, "MovieList: " + movieList.getTitle() + ", username: " + movieList.getUser().getUsername());
+                }
+                movieLists.addAll(movieLists);
+                movieAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
