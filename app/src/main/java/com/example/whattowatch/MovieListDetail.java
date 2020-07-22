636ecb8b32a3c59487eebfca3579c3bd801e2f32
Manny810 <mannyfavela2000@gmail.com>
2020-07-22 14:21:16 -0500

    creating movie list detail activity

---
+++ app/src/main/java/com/example/whattowatch/MovieListDetail.java
@@ -0,0 +1,14 @@
+package com.example.whattowatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieListDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list_detail);
+    }
+}
