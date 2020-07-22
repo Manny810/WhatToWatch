74188f6f414183a341927dd04e3220dcdfbd6f9d
Manny810 <mannyfavela2000@gmail.com>
2020-07-22 16:09:25 -0500

    added recyclerView for movieList specific recommendations, so recommendations for movieList can now be seen in movieListDetail

--- app/src/main/java/com/example/whattowatch/MovieListDetail.java
+++ app/src/main/java/com/example/whattowatch/MovieListDetail.java
@@ -9,20 +9,29 @@ import android.text.format.DateUtils;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.whattowatch.adapters.RecommenderAdapter;
 import com.example.whattowatch.models.Movie;
 import com.example.whattowatch.models.MovieList;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import okhttp3.Headers;
+
 public class MovieListDetail extends AppCompatActivity {
     public static final String TAG = "MovieListDetail";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/%s/recommendations?api_key=557bf444fa647aa33e0e1a2de0317f55&language=en-US&page=1";
 
     TextView tvListTitle;
     TextView tvListSize;
@@ -30,11 +39,16 @@ public class MovieListDetail extends AppCompatActivity {
     TextView tvRecommendationSize;
 
     RecyclerView rvMovieListDetail;
+    RecyclerView rvMovieListRecommendations;
 
     MovieList movieList;
     List<Movie> movies;
+    List<Movie> movieRecs;
 
     RecommenderAdapter movieAdapter;
+    RecommenderAdapter movieRecAdapter;
+
+    AsyncHttpClient client = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +60,55 @@ public class MovieListDetail extends AppCompatActivity {
         tvListCreatedAt = findViewById(R.id.tvListCreatedAt);
         tvRecommendationSize = findViewById(R.id.tvRecommendationSize);
         rvMovieListDetail = findViewById(R.id.rvMovieListDetail);
+        rvMovieListRecommendations = findViewById(R.id.rvMovieListRecommendations);
 
         // unwrap the movieList passed in via intent, using its simple name as a key
         movieList = (MovieList) Parcels.unwrap(getIntent().getParcelableExtra(MovieList.class.getSimpleName()));
 
         assert movieList != null;
         movies = movieList.getListOfMovies();
+        movieRecs = new ArrayList<>();
         movieAdapter = new RecommenderAdapter(this, movies);
+        movieRecAdapter = new RecommenderAdapter(this, movieRecs);
         rvMovieListDetail.setAdapter(movieAdapter);
+        rvMovieListRecommendations.setAdapter(movieRecAdapter);
         rvMovieListDetail.setLayoutManager(new LinearLayoutManager(this));
+        rvMovieListRecommendations.setLayoutManager(new LinearLayoutManager(this));
 
         tvListTitle.setText(movieList.getTitle());
         tvListSize.setText(String.format("Number of Movies: %d", movies.size()));
         tvListCreatedAt.setText(String.format("Created %s", getRelativeTimeAgo(movieList.getCreatedAt().toString())));
 
+        getRecommendations();
+
+    }
+
+    private void getRecommendations() {
+        for (Movie movie : movies) {
+            client.get(String.format(NOW_PLAYING_URL, movie.getID()), new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Headers headers, JSON json) {
+                    Log.d(TAG, "onSuccess");
+                    JSONObject jsonObject = json.jsonObject;
+
+                    try {
+                        JSONArray results = jsonObject.getJSONArray("results");
+                        Movie movie = Movie.fromJsonObject(results.getJSONObject(0));
+                        movieRecs.add(movie);
+                        movieRecAdapter.notifyDataSetChanged();
+
+                    } catch (JSONException ex) {
+                        Log.e(TAG, "Hit Json Exception", ex);
+                    }
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                    Log.e(TAG, "onFailure called");
+                }
+            });
+        }
     }
 
     // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
