a8940fd05b7cffcb1cec70f13872743f6ba5ceef
Manny810 <mannyfavela2000@gmail.com>
2020-07-29 15:30:30 -0500

    finished implementing the movie trailer activity and it's view after importing youtube jar file

--- app/src/main/java/com/example/whattowatch/MovieTrailerActivity.java
+++ app/src/main/java/com/example/whattowatch/MovieTrailerActivity.java
@@ -1,14 +1,43 @@
 package com.example.whattowatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
-public class MovieTrailerActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
+    public static final String YOUTUBE_API_KEY = "AIzaSyB_9M-T1DD9puOBf07gKVyMjlxhVQZpKu0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_trailer);
+
+        // temporary test video id -- TODO replace with movie trailer video id
+        final String videoId = getIntent().getStringExtra("youtubePath");
+
+        // resolve the player view from the layout
+        YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        // initialize with API key stored in secrets.xml
+        playerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(videoId);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                YouTubeInitializationResult youTubeInitializationResult) {
+                // log the error
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player");
+            }
+        });
     }
 }
