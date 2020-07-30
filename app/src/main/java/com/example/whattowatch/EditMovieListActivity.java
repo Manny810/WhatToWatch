fecb4a77cb730d7a74ed1920ee60028d0974207f
Manny810 <mannyfavela2000@gmail.com>
2020-07-30 11:09:16 -0500

    creating an edit movie list activity to edit current movie lists

---
+++ app/src/main/java/com/example/whattowatch/EditMovieListActivity.java
@@ -0,0 +1,14 @@
+package com.example.whattowatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditMovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_movie_list);
+    }
+}
